73604ffb1ca7c6441d7454ac3e0c1aed11fb606e
farhanach27 <59922085+farhanach27@users.noreply.github.com>
2021-07-21 12:13:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { setLoginStatus, startGetUserDetails } from './actions/userAuthActions';
+import { setLoginStatus } from './actions/userAuthActions';
 import NavBar from './components/commons/NavBar';
-import { startGetAllCustomers } from './actions/customerActions';
-import { startGetAllProducts } from './actions/productsActions';
-import { startGetAllBills } from './actions/billsActions';
 
 const App = () => {
 
@@ -15,10 +12,6 @@ const App = () => {
   useEffect (() => {
     if(localStorage.getItem('token')){
       dispatch(setLoginStatus())
-      dispatch(startGetUserDetails());
-      dispatch(startGetAllCustomers());
-      dispatch(startGetAllProducts());
-      dispatch(startGetAllBills())
     }
   })
 
