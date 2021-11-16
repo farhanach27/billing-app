8e1a411bc112f61d81cb5a88c10ff36a98db2df5
farhanach27 <farhanach27@gmail.com>
2021-11-16 17:26:01 +0530

    test

--- src/components/helpers/PublicRoute.js
+++ src/components/helpers/PublicRoute.js
@@ -1,25 +1,22 @@
 import { Redirect, Route } from "react-router-dom";
 
-
-const PublicRoute = ({component : Component, ...rest}) => {
-
-    return (
-        <Route
-            {...rest}
-
-            render={(props) => {
-                return !localStorage.getItem('token') ? (
-                    <Component {...props} />
-                ) : (
-                    <Redirect 
-                        to={{
-                            pathname : '/dashboard'
-                        }} 
-                    />
-                )
+const PublicRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return !localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/dashboard",
             }}
-        />
-    )
-}
+          />
+        );
+      }}
+    />
+  );
+};
 
 export default PublicRoute;
